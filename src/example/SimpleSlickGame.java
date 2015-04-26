0a141c0f99ae84d0f811320bfa2c66155eb765da
mtmarci <mtmarci@gmail.com>
2015-04-26 22:19:04 +0200

    Temporary graphics modification

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -6,6 +6,7 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.*;
 
 public class SimpleSlickGame extends BasicGame
 {
@@ -16,6 +17,7 @@ public class SimpleSlickGame extends BasicGame
 	public int typeOfNextBlock = 0;
 	public boolean tetrisPause = false;
 	public Block current = new Block(2);
+	
 
 	
 	public SimpleSlickGame(String gamename)
@@ -32,6 +34,8 @@ public class SimpleSlickGame extends BasicGame
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
+	
+		
 		if(tetrisOver == false){
 			//g.drawString("Hello World!", 250, 200);
 			}
@@ -40,6 +44,18 @@ public class SimpleSlickGame extends BasicGame
 			g.drawString(current.form[1][0]+" "+current.form[1][1]+" "+current.form[1][2]+" "+current.form[1][3], 200, 250);
 			g.drawString(current.form[2][0]+" "+current.form[2][1]+" "+current.form[2][2]+" "+current.form[2][3], 200, 300);
 			g.drawString(current.form[3][0]+" "+current.form[3][1]+" "+current.form[3][2]+" "+current.form[3][3], 200, 350);
+			
+			Rectangle r = new Rectangle(0,0,100,100);
+			
+			for(int x=0; x < 10; x++){
+				for(int y=0; y< 20; y++){
+					if(blockMatrix[x][y] == true){
+			g.fillRect(x*11, y*11, 10, 10);
+					}
+				}
+			}
+			
+			
 
 	}
 
@@ -66,6 +82,7 @@ public class SimpleSlickGame extends BasicGame
 				tempResult = false;
 			}
 		}
+		
 		
 		if(tempResult == true){
 		return(true);
