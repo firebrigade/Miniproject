1c4ce99ac23566958668d78d6530ec0a21177cd2
firebrigade <nkhokh13@student.aau.dk>
2015-04-29 10:29:56 +0200

    Basics of the control class

---
+++ src/example/Controls.java
@@ -0,0 +1,23 @@
+package example;
+
+import java.util.Scanner;
+
+public class Controls {
+	
+	char rotation = 'r'; 
+	Scanner readKey = new Scanner(System.in);
+	Controls (char tempRotation){
+		rotation = tempRotation;
+	}
+Controls (){
+		
+	}
+
+	void get(){
+		//if(readKey.hasNext(rotation))
+		{
+			
+		}
+	}
+	
+}
