f4f2691f33de000ed3a34a42a288ef74ae69c6bd
mtmarci <mtmarci@gmail.com>
2015-05-07 15:06:50 +0200

    Finished commenting

--- src/example/Button.java
+++ src/example/Button.java
@@ -16,11 +16,11 @@ public class Button {
 	Color buttonColor = new Color(0,200,100);  //three different colors to the buttons - buttonColor,2,3
 	Color buttonColor2 = new Color(0,255,50);   // these are also the different states of the game, normal, selected, inactive
 	Color buttonColor3 = new Color(150,150,150);
-	String label;
-	boolean selectable = true;
-	boolean isSelected = false;
+	String label; //The label of the button
+	boolean selectable = true; //Indicates if the button will be available to press
+	boolean isSelected = false; //Indicates if this is the currently selected button
 	
-	Button(int tempPosY, String tempLabel){
+	Button(int tempPosY, String tempLabel){ //Initializing the button in the constructor
 		posY = tempPosY;
 		label = tempLabel;
 		
@@ -31,20 +31,20 @@ public class Button {
 			if(selectable == true && isSelected == false){   //the default color
 				g.setColor(buttonColor);
 			}
-			else if(selectable == true && isSelected == true){
+			else if(selectable == true && isSelected == true){ //Sets the color for the active button
 				g.setColor(buttonColor2);
 			}
-			else if(selectable == false){
+			else if(selectable == false){ //Sets the color for the disabled button
 				g.setColor(buttonColor3);
 			}
 				
-				g.fillRect(posX, posY, sizeX, sizeY); // 
+				g.fillRect(posX, posY, sizeX, sizeY); //Draws the button
 				g.setColor(new Color(255,255,255));
-				g.drawString(label, posX+10, posY+10);
+				g.drawString(label, posX+10, posY+10); //Writes the label
 			
 	}
 	
-	boolean isClicked(){
+	boolean isClicked(){ //If the button is selected (and selectable) and the user has pressed enter then this will return true and trigger actions in the main function.
 		if(isSelected == true && selectable == true && input.isKeyDown(Input.KEY_ENTER)){
 			return true;
 		}
